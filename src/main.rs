b37a622bb6c3b4ff2f5ca1fde203738a37d4c821
Markus Mayer <widemeadows@gmail.com>
2023-04-26 22:35:08 +0200

    Add warp

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
 use tracing::info;
+use warp::Filter;
 
 mod commands;
 mod logging;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     dotenvy::dotenv().ok();
     let matches = commands::build_command().get_matches();
     logging::initialize_from_matches(&matches);
     info!("Hi. 👋");
+
+    // GET /hello/warp => 200 OK with body "Hello, warp!"
+    let hello = warp::path!("hello" / String).map(|name| format!("Hello, {}!", name));
+
+    warp::serve(hello).run(([127, 0, 0, 1], 3030)).await;
 
     info!("Bye. 👋");
 }
