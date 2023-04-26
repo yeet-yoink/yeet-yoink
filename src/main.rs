89c7a76e47e70ac1f955a054fa008b9c769b055d
Markus Mayer <widemeadows@gmail.com>
2023-04-26 23:37:42 +0200

    Add graceful shutdown to server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use tokio::sync::broadcast;
 use tracing::info;
 use warp::Filter;
 
@@ -9,12 +10,50 @@ async fn main() {
     dotenvy::dotenv().ok();
     let matches = commands::build_command().get_matches();
     logging::initialize_from_matches(&matches);
+
+    // Provide a signal that can be used to shut down the server.
+    let (shutdown_tx, mut shutdown_rx) = broadcast::channel::<()>(1);
+    let shutdown_filter = warp::any().map(move || shutdown_tx.clone());
+
     info!("Hi. 👋");
 
     // GET /hello/warp => 200 OK with body "Hello, warp!"
-    let hello = warp::path!("hello" / String).map(|name| format!("Hello, {}!", name));
+    let hello = warp::path!("hello" / String).and_then(hello);
 
-    warp::serve(hello).run(([127, 0, 0, 1], 3030)).await;
+    // POST /stop to shut down the server.
+    let shutdown = warp::post()
+        .and(warp::path("stop"))
+        .and(warp::path::end())
+        .and(shutdown_filter)
+        .and_then(shutdown);
+
+    let (_http_addr, http_warp) = warp::serve(hello.or(shutdown)).bind_with_graceful_shutdown(
+        ([127, 0, 0, 1], 2080),
+        async move {
+            shutdown_rx.recv().await.ok();
+        },
+    );
+
+    http_warp.await;
 
     info!("Bye. 👋");
+}
+
+/// Responds with the caller's name.
+///
+/// ```http
+/// GET /hello/name
+/// ```
+async fn hello(name: String) -> Result<impl warp::Reply, warp::Rejection> {
+    Ok(format!("Hello, {}!", name))
+}
+
+/// Initiates a graceful shutdown.
+///
+/// ```http
+/// POST /stop
+/// ```
+async fn shutdown(tx: broadcast::Sender<()>) -> Result<impl warp::Reply, warp::Rejection> {
+    tx.send(()).ok();
+    Ok(warp::reply::reply())
 }
