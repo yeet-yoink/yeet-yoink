99e315b07b1d0d21fb9cc296c3513ce1fddf8dc3
Markus Mayer <widemeadows@gmail.com>
2023-06-17 00:21:24 +0200

    Remove Hash<T, D> inner type from hashes

--- src/backbone/hash.rs
+++ src/backbone/hash.rs
@@ -1,63 +1,37 @@
 use sha2::Digest;
 
 /// An MD5 hash.
-pub struct HashMd5(Hash<md5::Context>);
+pub struct HashMd5(md5::Context);
 
 /// A SHA-256 hash.
-pub struct HashSha256(Hash<sha2::Sha256>);
+pub struct HashSha256(sha2::Sha256);
 
 impl HashMd5 {
     pub fn new() -> Self {
-        Self(Hash::new(md5::Context::new()))
+        Self(md5::Context::new())
     }
 
     pub fn update(&mut self, chunk: &[u8]) {
-        self.0.update(move |md5| md5.consume(chunk))
+        self.0.consume(chunk)
     }
 
     pub fn finalize(self) -> md5::Digest {
-        self.0.finalize(move |h| h.compute())
+        self.0.compute()
     }
 }
 
 impl HashSha256 {
     pub fn new() -> Self {
-        Self(Hash::new(sha2::Sha256::new()))
+        Self(sha2::Sha256::new())
     }
 
     pub fn update(&mut self, chunk: &[u8]) {
-        self.0.update(move |h| h.update(chunk))
+        self.0.update(chunk)
     }
 
     pub fn finalize(self) -> [u8; 32] {
-        self.0.finalize(move |sha256| {
-            let mut hash = [0u8; 32];
-            sha256.finalize_into((&mut hash).into());
-            hash
-        })
-    }
-}
-
-/// The underlying hash implementation.
-#[derive(Debug)]
-struct Hash<T>(T);
-
-impl<T> Hash<T> {
-    pub fn new(hasher: T) -> Self {
-        Self(hasher)
-    }
-
-    fn update<F>(&mut self, mut f: F)
-    where
-        F: FnMut(&mut T) -> (),
-    {
-        f(&mut self.0)
-    }
-
-    fn finalize<F, D>(self, mut f: F) -> D
-    where
-        F: FnMut(T) -> D,
-    {
-        f(self.0)
+        let mut hash = [0u8; 32];
+        self.0.finalize_into((&mut hash).into());
+        hash
     }
 }
