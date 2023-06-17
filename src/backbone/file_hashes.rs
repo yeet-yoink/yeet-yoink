b7eb6002534a98dcab4e6f84aa92de0e8e5dd841
Markus Mayer <widemeadows@gmail.com>
2023-06-17 16:52:16 +0200

    Return GenericArray from SHA-256 calculation

--- src/backbone/file_hashes.rs
+++ src/backbone/file_hashes.rs
@@ -1,11 +1,12 @@
+use crate::backbone::hash::{Md5Digest, Sha256Digest};
 use std::fmt::{Debug, Display, Formatter};
 
 /// The calculated hashes of a file.
 pub struct FileHashes {
     /// The MD5 digest.
-    pub md5: md5::Digest,
+    pub md5: Md5Digest,
     /// The SHA-256 hash.
-    pub sha256: [u8; 32], // TODO: Replace with GenericArray
+    pub sha256: Sha256Digest,
 }
 
 impl Debug for FileHashes {
@@ -18,9 +19,9 @@ impl Display for FileHashes {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         write!(
             f,
-            "MD5 {md5:x}, SHA256 {sha256}",
+            "MD5 {md5:x}, SHA256 {sha256:x}",
             md5 = self.md5,
-            sha256 = hex::encode(self.sha256)
+            sha256 = self.sha256
         )
     }
 }
--- src/backbone/hash.rs
+++ src/backbone/hash.rs
@@ -1,3 +1,5 @@
+use sha2::digest::consts::U32;
+use sha2::digest::generic_array::{ArrayLength, GenericArray};
 use sha2::Digest;
 
 /// An MD5 hash.
@@ -5,6 +7,12 @@ pub struct HashMd5(md5::Context);
 
 /// A SHA-256 hash.
 pub struct HashSha256(sha2::Sha256);
+
+/// Alias for a SHA-256 hash digest.
+pub type Md5Digest = md5::Digest;
+
+/// Alias for a SHA-256 hash digest.
+pub type Sha256Digest = GenericArray<u8, U32>;
 
 impl HashMd5 {
     pub fn new() -> Self {
@@ -15,7 +23,7 @@ impl HashMd5 {
         self.0.consume(chunk)
     }
 
-    pub fn finalize(self) -> md5::Digest {
+    pub fn finalize(self) -> Md5Digest {
         self.0.compute()
     }
 }
@@ -29,9 +37,10 @@ impl HashSha256 {
         self.0.update(chunk)
     }
 
-    pub fn finalize(self) -> [u8; 32] {
+    pub fn finalize(self) -> Sha256Digest {
         let mut hash = [0u8; 32];
-        self.0.finalize_into((&mut hash).into());
+        let mut hash = GenericArray::from(hash);
+        self.0.finalize_into(&mut hash);
         hash
     }
 }
